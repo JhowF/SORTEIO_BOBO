e4ce93c4d700be087c4a65003215b31a4aa4168e
JhowF <jonathanferreirar@gmail.com>
2022-10-17 01:46:31 -0300

    Funcão_adicionar_ok_verificar_em_progresso_ainda

--- Script.js
+++ Script.js
@@ -1,6 +1,8 @@
 //Mudando no button "Verificar";
 
-const button_verificar = document.querySelector('#demo');
+let button_verificar = document.querySelector('#demo');
+
+let button_adicionar = document.querySelector('#adicionar');
 
 function mudar(){
 button_verificar.textContent="Vai dar Filipe🤣";
@@ -17,8 +19,70 @@ button_verificar.addEventListener('mouseout',voltar);
 
 let nome = document.querySelector('#User_name');
 
-console.log(nome)
-
 let caracteristicas = document.querySelectorAll('.carac_name');
 
-console.log(caracteristicas)
+
+
+//Config buttton adicionar
+
+listaGeral = []
+//Função para jogar os dados dentro da lista
+function caracValue(){
+
+  //Lista para inserir os arquivos em forma de lista dentro da lista geral
+  listaCarac = []
+
+  //Adicionando o nome na primeira posição dentro da lista
+  listaCarac.push(nome.value.toUpperCase())
+  for(i=0;i < 5; i++){
+    
+    listaCarac.push(caracteristicas[i].value.toUpperCase())
+    caracteristicas[i].value = ""
+    
+  }
+  
+  listaGeral.push(listaCarac)
+
+  //Zerando o input nome
+  nome.value = ""
+  console.log(listaGeral)
+}
+
+
+button_adicionar.addEventListener('click', caracValue);
+
+
+
+
+//Config buttton verificar
+const ListaCaracPessoaBoba = ['FELIZ', 'SORRIDENTE', 'ALEGRE',
+ 'ENGRAÇADO','PIADISTA', 'BOBO',
+  'BOBAO', 'SACANEADO', 'IDIOTA', 
+  'TRAPALHAO', 'BURRO', 'TAPADO',
+   'SINICO']
+
+   test = 'JONATHAN'
+
+   function buttonVericar(){
+
+   for(i=0; i < 6; i++){
+
+//    console.log(`${ListaCaracPessoaBoba[i]},  ${i}`)
+    
+ 
+    ListaCaracPessoaBoba.forEach(element => {
+      listaGeral.forEach(geral=>{
+        
+        if (element === geral[i]){
+       console.log('tem')}else{
+        console.log('nao tem')
+       }
+        
+      });
+      
+      
+    });
+  
+  }
+   }
+  button_verificar.addEventListener('click', buttonVericar);
